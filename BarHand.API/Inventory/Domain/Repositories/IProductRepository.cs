e787bd2c0667ac26c27c2ff21e096ca50217b506
Rojoxt <delamarzp@gmail.com>
2022-10-31 14:49:10 -0500

    feat(products): implemented products repository.

---
+++ BarHand.API/Inventory/Domain/Repositories/IProductRepository.cs
@@ -0,0 +1,12 @@
+﻿using BarHand.API.Inventory.Domain.Models;
+
+namespace BarHand.API.Inventory.Domain.Repositories;
+
+public interface IProductRepository
+{
+    Task<IEnumerable<Product>> ListAsync();
+    Task AddAsync(Product product);
+    Task<Product> FindByAsync(int id);
+    void Update(Product product);
+    void Remove(Product product);
+}
---
+++ BarHand.API/Inventory/Persistence/Repositories/ProductRepository.cs
@@ -0,0 +1,39 @@
+﻿using BarHand.API.Inventory.Domain.Models;
+using BarHand.API.Inventory.Domain.Repositories;
+using BarHand.API.Shared.Persistence.Contexts;
+using BarHand.API.Shared.Persistence.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace BarHand.API.Inventory.Persistence.Repositories;
+
+public class ProductRepository : BaseRepository , IProductRepository
+{
+    public ProductRepository(AppDbContext context) : base(context)
+    {
+    }
+
+    public async Task<IEnumerable<Product>> ListAsync()
+    {
+        return await _context.Products.ToListAsync();
+    }
+
+    public async Task AddAsync(Product product)
+    {
+        await _context.Products.AddAsync(product);
+    }
+
+    public async Task<Product> FindByAsync(int id)
+    {
+        return await _context.Products.FindAsync(id);
+    }
+
+    public void Update(Product product)
+    {
+        _context.Products.Update( product);
+    }
+
+    public void Remove(Product product)
+    {
+        _context.Products.Remove(product);
+    }
+}
