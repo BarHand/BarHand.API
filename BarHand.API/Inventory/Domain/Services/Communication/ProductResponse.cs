86174a3a8dd7056baa276ab8d368c28ddde57ada
Rojoxt <delamarzp@gmail.com>
2022-10-31 15:20:56 -0500

    feat(products): created product service standard response.

---
+++ BarHand.API/Inventory/Domain/Services/Communication/ProductResponse.cs
@@ -0,0 +1,15 @@
+﻿using BarHand.API.Inventory.Domain.Models;
+using BarHand.API.Shared.Domain.Services.Communication;
+
+namespace BarHand.API.Inventory.Domain.Services.Communication;
+
+public class ProductResponse: BaseResponse<Product>
+{
+    public ProductResponse(string message) : base(message)
+    {
+    }
+
+    public ProductResponse(Product resource) : base(resource)
+    {
+    }
+}
