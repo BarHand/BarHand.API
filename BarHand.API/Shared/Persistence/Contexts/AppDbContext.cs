ba5e4bd528d90d4202650743bd05881d05f78bfd
Rojoxt <delamarzp@gmail.com>
2022-10-31 13:31:14 -0500

    feat(products): added object-relational mapping constrain rules.

---
+++ BarHand.API/Shared/Persistence/Contexts/AppDbContext.cs
@@ -0,0 +1,27 @@
+﻿using BarHand.API.Inventory.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BarHand.API.Shared.Persistence.Contexts;
+
+public class AppDbContext : DbContext
+{
+    public DbSet<Product> Products { get; set; }
+    
+    public AppDbContext(DbContextOptions options) : base(options)
+    {
+    }
+
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+
+        //Products Configuration
+        builder.Entity<Product>().ToTable("Products");
+        builder.Entity<Product>().HasKey(p => p.Id);
+        builder.Entity<Product>().Property(p => p.Id)
+            .IsRequired().ValueGeneratedOnAdd();// is required
+        builder.Entity<Product>().Property(p => p.Name)
+            .IsRequired().HasMaxLength(30);//tamaño max de 30 
+    }
+}
