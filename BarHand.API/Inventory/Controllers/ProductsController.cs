eb4d7a88725f6a94c3e080bdb21ab359b158100b
Rojoxt <delamarzp@gmail.com>
2022-11-01 10:19:38 -0500

    feat(products): added initial action for products controller.

---
+++ BarHand.API/Inventory/Controllers/ProductsController.cs
@@ -0,0 +1,25 @@
+﻿using System.Net.Mime;
+using BarHand.API.Inventory.Domain.Models;
+using BarHand.API.Inventory.Domain.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BarHand.API.Inventory.Controllers;
+
+[ApiController]
+[Route("/api/v1/[controller]")]
+[Produces(MediaTypeNames.Application.Json)]
+public class ProductsController:ControllerBase
+{
+    private readonly IProductService _productService;
+
+    public ProductsController(IProductService productService)
+    {
+        _productService = productService;
+    }
+
+    public async Task<IEnumerable<Product>> GetAllAsync()
+    {
+        var products = await _productService.ListAsync();
+        return products;
+    }
+}
