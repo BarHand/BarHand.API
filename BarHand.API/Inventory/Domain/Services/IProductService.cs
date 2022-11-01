c604d2c38f68c94a62cdee611f02c43b0f5ac375
Rojoxt <delamarzp@gmail.com>
2022-11-01 09:49:10 -0500

    feat(products): implement product service.

---
+++ BarHand.API/Inventory/Domain/Services/IProductService.cs
@@ -0,0 +1,13 @@
+﻿using BarHand.API.Inventory.Domain.Models;
+using BarHand.API.Inventory.Domain.Services.Communication;
+
+namespace BarHand.API.Inventory.Domain.Services;
+
+public interface IProductService
+{
+    Task<IEnumerable<Product>> ListAsync();
+    Task<ProductResponse> SaveAsync(Product product);
+    Task<ProductResponse> UpdateAsync(int id, Product product);
+    Task<ProductResponse> DeleteAsync(int id);
+    
+}
