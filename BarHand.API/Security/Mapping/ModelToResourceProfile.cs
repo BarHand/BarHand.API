61367dae0d70ccb8c4487db8697fb4db95755407
Rojoxt <delamarzp@gmail.com>
2022-11-15 13:46:00 -0500

    update

--- BarHand.API/Security/Mapping/ModelToResourceProfile.cs
+++ BarHand.API/Security/Mapping/ModelToResourceProfile.cs
@@ -1,12 +1,6 @@
 ﻿using AutoMapper;
-using BarHand.API.Inventory.Domain.Models;
-using BarHand.API.Inventory.Resources;
 using BarHand.API.Security.Domain.Models;
 using BarHand.API.Security.Domain.Services.Communication;
-using BarHand.API.Stores.Domain.Models;
-using BarHand.API.Stores.Resources;
-using BarHand.API.Suppliers.Domain.Models;
-using BarHand.API.Suppliers.Resources;
 using Mecanillama.API.Security.Resources;
 
 namespace BarHand.API.Security.Mapping;
