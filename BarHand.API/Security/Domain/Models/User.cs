d191f17799352915aa00d82ff8c44083ed2bc10e
Rojoxt <delamarzp@gmail.com>
2022-11-15 13:18:24 -0500

    feat(security): added user entity

---
+++ BarHand.API/Security/Domain/Models/User.cs
@@ -0,0 +1,14 @@
+﻿using System.Text.Json.Serialization;
+
+namespace BarHand.API.Security.Domain.Models;
+
+public class User
+{
+    public long Id { get; set; }
+    public string Email { get; set; }
+    public string Username { get; set; }
+    
+    //Possible Role Attribute
+    [JsonIgnore]
+    public string PasswordHash { get; set; }
+}
