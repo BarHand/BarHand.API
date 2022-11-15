e4d576b33425c143fef43ec8d00c73aa3424b91f
Rojoxt <delamarzp@gmail.com>
2022-11-15 13:22:13 -0500

    feat(security): added user repository

--- BarHand.API/Security/Domain/Models/User.cs
+++ BarHand.API/Security/Domain/Models/User.cs
@@ -5,7 +5,8 @@ namespace BarHand.API.Security.Domain.Models;
 public class User
 {
     public long Id { get; set; }
-    public string Email { get; set; }
+    public string Name { get; set; }
+    public string LastName { get; set; }
     public string Username { get; set; }
     
     //Possible Role Attribute
---
+++ BarHand.API/Security/Domain/Repositories/IUserRepository.cs
@@ -0,0 +1,15 @@
+﻿using BarHand.API.Security.Domain.Models;
+
+namespace BarHand.API.Security.Domain.Repositories;
+
+public interface IUserRepository
+{
+    Task<IEnumerable<User>> ListAsync();
+    Task AddAsync(User user);
+    Task<User> FindByIdAsync(long id);
+    Task<User> FindByEmailAsync(string email);
+    bool ExistsByEmail(string email);
+    User FindById(long id);
+    void Update(User user);
+    void Remove(User user);
+}
