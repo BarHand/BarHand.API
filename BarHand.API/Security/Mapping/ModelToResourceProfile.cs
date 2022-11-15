f70445004920336647a349102e131df7e7509855
Rojoxt <delamarzp@gmail.com>
2022-11-15 13:44:46 -0500

    feat(security): added mapping profile

---
+++ BarHand.API/Security/Mapping/ModelToResourceProfile.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using BarHand.API.Inventory.Domain.Models;
+using BarHand.API.Inventory.Resources;
+using BarHand.API.Security.Domain.Models;
+using BarHand.API.Security.Domain.Services.Communication;
+using BarHand.API.Stores.Domain.Models;
+using BarHand.API.Stores.Resources;
+using BarHand.API.Suppliers.Domain.Models;
+using BarHand.API.Suppliers.Resources;
+using Mecanillama.API.Security.Resources;
+
+namespace BarHand.API.Security.Mapping;
+
+public class ModelToResourceProfile:Profile
+{
+    public ModelToResourceProfile()
+    {
+        CreateMap<User, AuthenticateResponse>();
+        CreateMap<User, UserResource>();
+    }
+}
---
+++ BarHand.API/Security/Mapping/ResourceToModelProfile.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+using BarHand.API.Inventory.Domain.Models;
+using BarHand.API.Inventory.Resources;
+using BarHand.API.Security.Domain.Models;
+using BarHand.API.Security.Domain.Services.Communication;
+using BarHand.API.Stores.Domain.Models;
+using BarHand.API.Stores.Resources;
+using BarHand.API.Suppliers.Domain.Models;
+using BarHand.API.Suppliers.Resources;
+
+namespace BarHand.API.Security.Mapping;
+
+public class ResourceToModelProfile:Profile
+{
+    public ResourceToModelProfile()
+    {
+        CreateMap<RegisterRequest, User>();
+
+        CreateMap<UpdateRequest, User>()
+            .ForAllMembers(options => options.Condition(
+                (source, target, property) =>
+                {
+                    if (property == null) return false;
+                    if (property.GetType() == typeof(string) && string.IsNullOrEmpty((string)property)) return false;
+                    return true;
+                }
+            ));
+    }
+}
