95de19c0940a2e04ef78eed1a6ed7730363be5df
Rojoxt <delamarzp@gmail.com>
2022-11-15 13:34:08 -0500

    feat(security): added User services

---
+++ BarHand.API/Security/Domain/Services/Communication/AuthenticateResponse.cs
@@ -0,0 +1,11 @@
+﻿namespace BarHand.API.Security.Domain.Services.Communication;
+
+public class AuthenticateResponse
+{
+    public long Id { get; set; }
+    public string Name { get; set; }
+    public string LastName { get; set; }
+    public string Username { get; set; }
+    public string Token { get; set; }
+    
+}
---
+++ BarHand.API/Security/Domain/Services/Communication/UpdateRequest.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BarHand.API.Security.Domain.Services.Communication;
+
+public class UpdateRequest
+{
+    public string Name { get; set; }
+    public string LastName { get; set; }
+    public string Username { get; set; }
+    public string Password { get; set; }
+}
