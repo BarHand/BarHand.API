083df2e53859e8393a478a80d694fcb72826e59b
Rojoxt <delamarzp@gmail.com>
2022-11-15 14:21:35 -0500

    feat(security): implemented token handler.

---
+++ BarHand.API/Security/Authorization/Handlers/Interfaces/IJwtHandler.cs
@@ -0,0 +1,9 @@
+﻿using BarHand.API.Security.Domain.Models;
+
+namespace BarHand.API.Security.Authorization.Handlers.Interfaces;
+
+public interface IJwtHandler
+{
+    public string GenerateToken(User user);
+    public int? ValidateToken(string token);
+}
