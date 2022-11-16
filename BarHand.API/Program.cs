8ee8b29a3d9b2ebb67c2ec29f8c0ba4eb8b720c1
Rojoxt <U20201b032@upc.edu.pe>
2022-11-16 15:34:34 -0500

    fix: fixed configuration deploy

--- BarHand.API/Program.cs
+++ BarHand.API/Program.cs
@@ -78,7 +78,7 @@ builder.Services.AddSwaggerGen(options=>
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 
 builder.Services.AddDbContext<AppDbContext>(
-    options=>options.UseMySQL(connectionString)
+    options=>options.UseNpgsql(connectionString)
         .LogTo(Console.WriteLine,LogLevel.Information)
         .EnableSensitiveDataLogging()
         .EnableDetailedErrors());
