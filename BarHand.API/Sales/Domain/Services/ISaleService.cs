a616888741f682902863d91482c66a85801015b5
AlessandroChincha <89095463+AlessandroChincha@users.noreply.github.com>
2022-11-21 20:26:35 -0500

    feat: Sale service

---
+++ BarHand.API/Sales/Domain/Services/ISaleService.cs
@@ -0,0 +1,11 @@
+using BarHand.API.Sales.Domain.Models;
+using BarHand.API.Sales.Domain.Services.Communication;
+
+namespace BarHand.API.Sales.Domain.Services;
+
+public interface ISaleService{
+    Task<IEnumerable<Sales>> ListAsync();
+    Task<SaleResponse> SaveAsync(Sales sale);
+    Task<SaleResponse> UpdateAsync(int id, Sales sale);
+    Task<SaleResponse> DeleteAsync(int id)
+}
