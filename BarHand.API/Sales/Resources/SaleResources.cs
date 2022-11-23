8cd3f33f93bbef6113023c6a83f79529b6762ff8
AlessandroChincha <89095463+AlessandroChincha@users.noreply.github.com>
2022-11-23 13:04:09 -0500

    feat: Resource update

--- BarHand.API/Sales/Resources/SaleResources.cs
+++ BarHand.API/Sales/Resources/SaleResources.cs
@@ -7,8 +7,9 @@ namespace BarHand.API.Sales.Resources;
 public class SaleResource{
     public int Id {get; set;}
     public long IdStore {get; set;}
-    public long IdSupplier {get; set;}
+    public long IdProduct {get; set;}
     public bool Available {get;set;}
+    public decimal Stars {get;set;}
     public InventoryResource Product {get;set}
     public StoreResource Store {get;set;}
 }
