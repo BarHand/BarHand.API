9bd450f7e4dfdc2758a97fad426d8de21a1602d6
Rojoxt <u202010840@upc.edu.pe>
2022-11-23 15:25:08 -0500

    feat(salesOrder): added orderService interface.

---
+++ BarHand.API/SalesOrders/Services/IOrderService.cs
@@ -0,0 +1,14 @@
+﻿using BarHand.API.SalesOrders.Domain.Models;
+using BarHand.API.SalesOrders.Services.Communication;
+
+namespace BarHand.API.SalesOrders.Services;
+
+public interface IOrderService
+{
+    Task<IEnumerable<Order>> ListAsync();
+    Task<IEnumerable<Order>> ListByStoreIdAsync(long storeId);
+    Task<OrderResponse> GetByIdAsync(long id);
+    Task<OrderResponse> SaveAsync(Order order);
+    Task<OrderResponse> UpdateAsync( long id,Order product);
+    Task<OrderResponse> DeleteAsync(long id);
+}
