1140cab8d2d813ffced9e045c150b6335d9c3027
Rojoxt <u202010840@upc.edu.pe>
2022-11-23 15:14:34 -0500

    feat(salesOrder): added order detail interface repository.

---
+++ BarHand.API/SalesOrders/Domain/Repositories/IOrderDetailRepository.cs
@@ -0,0 +1,13 @@
+﻿using BarHand.API.SalesOrders.Domain.Models;
+
+namespace BarHand.API.SalesOrders.Domain.Repositories;
+
+public interface IOrderDetailRepository
+{
+    Task<IEnumerable<OrderDetail>> ListAsync();
+    Task AddAsync(OrderDetail orderDetail);
+    Task<OrderDetail> FindByIdAsync(long id);
+    Task<IEnumerable<OrderDetail>> FindByOrderIdAsync(long orderId);
+    void Update(OrderDetail orderDetail);
+    void Remove(OrderDetail orderDetail);
+}
