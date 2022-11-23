1430c88cec9bbeede791be56db3f4840573e2b96
AlessandroChincha <89095463+AlessandroChincha@users.noreply.github.com>
2022-11-23 13:03:51 -0500

    feat: Sale update

--- BarHand.API/Sales/Domain/Models/Sale.cs
+++ BarHand.API/Sales/Domain/Models/Sale.cs
@@ -2,5 +2,8 @@ namespace BarHand.API.Sales.Domain.Models;
 
 public class Sales{
     public int Id {get; set;}
-
+    public long IdStore {get; set;}
+    public long IdProduct {get; set;}
+    public bool Available {get;set;}
+    public decimal Stars {get;set;}
 }
