de2a3a5f5d67209ead28b3d1391bcf0d09261d53
Rojoxt <u201920124@upc.edu.pe>
2022-11-23 16:03:49 -0500

    feat(salesOrder): added order resource .

---
+++ BarHand.API/SalesOrders/Resources/OrderResource.cs
@@ -0,0 +1,10 @@
+﻿using BarHand.API.Stores.Resources;
+
+namespace BarHand.API.SalesOrders.Resources;
+
+public class OrderResource
+{
+    public long Id { get; set; }
+    public long StoreId { get; set; }
+    public StoreResource Store { get; set; }
+}
