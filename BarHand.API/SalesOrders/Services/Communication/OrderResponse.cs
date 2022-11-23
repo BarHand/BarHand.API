4ab9c4b43807abbd3706e0c2014dc3b136a7e66e
Rojoxt <u202010840@upc.edu.pe>
2022-11-23 15:20:36 -0500

    feat(salesOrder): added order response.

---
+++ BarHand.API/SalesOrders/Services/Communication/OrderResponse.cs
@@ -0,0 +1,15 @@
+﻿using BarHand.API.SalesOrders.Domain.Models;
+using BarHand.API.Shared.Domain.Services.Communication;
+
+namespace BarHand.API.SalesOrders.Services.Communication;
+
+public class OrderResponse:BaseResponse<Order>
+{
+    public OrderResponse(string message) : base(message)
+    {
+    }
+
+    public OrderResponse(Order resource) : base(resource)
+    {
+    }
+}
