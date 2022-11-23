3479c63c38c7d873dbd29074ca7f3550461c960c
Rojoxt <u201920124@upc.edu.pe>
2022-11-23 16:03:37 -0500

    feat(salesOrder): added order detail resource .

---
+++ BarHand.API/SalesOrders/Resources/OrderDetailResource.cs
@@ -0,0 +1,10 @@
+﻿namespace BarHand.API.SalesOrders.Resources;
+
+public class OrderDetailResource
+{
+    public long Id { get; set; }
+    public int Quantity { get; set; }
+    public int QuotedPrice { get; set; }
+    public long OrderId { get; set; }
+    public long ProductId { get; set; }
+}
