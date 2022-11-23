c66bcce53b61bb7199834d49a4605ae99e0ccffb
Rojoxt <u201920124@upc.edu.pe>
2022-11-23 16:03:59 -0500

    feat(salesOrder): added save order detail resource .

---
+++ BarHand.API/SalesOrders/Resources/SaveOrderDetailResource.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BarHand.API.SalesOrders.Resources;
+
+public class SaveOrderDetailResource
+{
+    [Required]
+    public int Quantity { get; set; }
+    [Required]
+    public int QuotedPrice { get; set; }
+    [Required]
+    public long OrderId { get; set; }
+    [Required]
+    public long ProductId { get; set; }
+}
