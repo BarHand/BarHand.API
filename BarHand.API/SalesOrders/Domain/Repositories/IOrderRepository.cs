b6bccc04d30e168075b0b95f0b18a91bec69712d
Rojoxt <u202010840@upc.edu.pe>
2022-11-23 15:14:23 -0500

    feat(salesOrder): added order interface repository.

---
+++ BarHand.API/SalesOrders/Domain/Repositories/IOrderRepository.cs
@@ -0,0 +1,13 @@
+﻿using BarHand.API.SalesOrders.Domain.Models;
+
+namespace BarHand.API.SalesOrders.Domain.Repositories;
+
+public interface IOrderRepository
+{
+    Task<IEnumerable<Order>> ListAsync();
+    Task AddAsync(Order order);
+    Task<Order> FindByIdAsync(long id);
+    Task<IEnumerable<Order>> FindByStoreIdAsync(long storeId);
+    void Update(Order product);
+    void Remove(Order order);
+}
