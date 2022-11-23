0ce38ca7e20242b01736c1d3322d62e24f4bd3b8
AlessandroChincha <89095463+AlessandroChincha@users.noreply.github.com>
2022-11-23 12:52:08 -0500

    feat: Sale save resource

--- BarHand.API/Sales/Resources/SaleResources.cs
+++ BarHand.API/Sales/Resources/SaleResources.cs
@@ -1,6 +1,6 @@
 using BarHand.API.Suppliers.Resources;
 using BarHand.API.Store.Resources;
-
+using BarHand.API.Inventory.Resources;
 namespace BarHand.API.Sales.Resources;
 
 
@@ -9,7 +9,6 @@ public class SaleResource{
     public long IdStore {get; set;}
     public long IdSupplier {get; set;}
     public bool Available {get;set;}
-
-    public SupplierResource Supplier {get;set;}
+    public InventoryResource Product {get;set}
     public StoreResource Store {get;set;}
 }
